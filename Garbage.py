e5543f666f7d333ecb71bb2a483511fb4a04db87
mayur <handemyckey@gmail.com>
2020-02-25 12:54:05 +0530

    type

--- Garbage.py
+++ Garbage.py
@@ -1,7 +1,10 @@
 import _ctypes
 import gc
+
+
 def ref_count(address):
     return _ctypes.c_long.from_address(address).value
+
 
 def object_by_id(object_id):
     for obj in gc.get_objects():
@@ -10,16 +13,21 @@ def object_by_id(object_id):
         else:
             print("Not Found")
 
+
 class A:
+
+
     def __init__(self):
         self.b = B(self)
-        print('A : self: {0}, b: {1}'.format(hex(id(self)),hex(id(self.b))))
+        print('A : self: {0}, b: {1}'.format(hex(id(self)), hex(id(self.b))))
+
 
 class B:
 
-     def __init__(self,a):
-         self.a = a
-         print('B: self: {0}, a: {1}'.format(hex(id(self)),hex(id(self.a))))
+    def __init__(self, a):
+        self.a = a
+        print('B: self: {0}, a: {1}'.format(hex(id(self)), hex(id(self.a))))
+
 
 gc.disable()
 gc.collect()
---
+++ object mutability.py
@@ -0,0 +1,19 @@
+my_list = (1,2,3) #tuple
+print(my_list)
+
+a = [1,2] #list
+b = [3,4] #list
+my_list= (a,b) #tuple
+
+a.append(5)
+b.append(6)
+
+print(my_list)
+print(id(my_list))
+print("\n\n")
+
+
+my_list = [1,2,3,4]
+print(type(my_list))
+
+print(id(my_list))
